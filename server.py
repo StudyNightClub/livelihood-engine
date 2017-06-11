3b87fac6efd337a8c70d0d694cfcb6d189d6b8e5
Ching Lin <ikafire1230@gmail.com>
2017-06-12 00:57:01 +0800

    [FEATURE] Integrate with mapplotter.

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import logging
 from http import HTTPStatus
 from flask import Flask, request, json
+from mapplotter import MapPlotter
 
 from chatbot_client import ChatbotClient, NotificationCategory
 import event_filter
@@ -12,27 +13,35 @@ from ldb_client import LivelihoodDbClient
 from service_location import ServiceLocation
 from udb_client import UserDbClient
 
+
 VERSION = 'v0.0.0'
 app = Flask(__name__)
+
 
 locations = ServiceLocation()
 if not locations.is_all_set():
     logging.error(locations.__dict__)
     raise EnvironmentError('Environment variables are incomplete.')
 
+
 livelihood = LivelihoodDbClient(locations.ldb_url)
 chatbot = ChatbotClient(locations.chatbot_url)
 users = UserDbClient(locations.udb_url, locations.udb_token)
+plotter = MapPlotter(locations.ldb_url, locations.map_url)
+
 
 logging.basicConfig(level=logging.DEBUG)
+
 
 @app.route('/')
 def greet():
     return json.jsonify({'greetings': 'You\'re accessing livelihood engine version {}'.format(VERSION)})
 
+
 @app.route('/get_map/<string:user_id>')
 def get_map(user_id):
     return important_images.get_image()
+
 
 @app.route('/notify_here/<string:user_id>', methods=['POST'])
 def notify_here(user_id):
@@ -46,23 +55,39 @@ def notify_here(user_id):
 
     events = get_events_of_tomorrow()
     events = event_filter.nearby_events(events, lat, lon)
+    logging.info('notify_here events {}'.format(events))
+
+    water_map, power_map, road_map = get_maps(events)
     chatbot.push_notification(user_id, NotificationCategory.USER_REQUESTED,
-        events, important_images.get_image(), important_images.get_image(),
-        important_images.get_image())
+        events, water_map, power_map, road_map)
     return json.jsonify({})
+
 
 @app.route('/notify_interest/<string:user_id>', methods=['POST'])
 def notify_interest(user_id):
     return user_id
 
+
 @app.route('/notify_all/<string:user_id>', methods=['POST'])
 def notify_all(user_id):
     events = get_events_of_tomorrow()
+    water_map, power_map, road_map = get_maps(events)
     chatbot.push_notification(user_id, NotificationCategory.BROADCAST,
-        events, important_images.get_image(), important_images.get_image(),
-        important_images.get_image())
+        events, water_map, power_map, road_map)
     return json.jsonify({})
+
 
 def get_events_of_tomorrow():
     tomorrow = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
     return livelihood.get_events({ 'after': tomorrow, 'before': tomorrow })
+
+
+def get_event_ids(events, event_type):
+    return [e['id'] for e in events if e['type'] == event_type]
+
+
+def get_maps(events):
+    water_map = plotter.drawMarkerById(get_event_ids(events, 'water'), body)
+    power_map = plotter.drawMarkerById(get_event_ids(events, 'power'), body)
+    road_map = plotter.drawMarkerById(get_event_ids(events, 'road'), body)
+    return water_map, power_map, road_map
