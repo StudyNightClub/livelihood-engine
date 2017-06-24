1c8586202d601a3932272f5dbfa011aee0546415
Ching Lin <ikafire1230@gmail.com>
2017-06-25 03:39:11 +0800

    [TASK] Cleanup unused code.

--- event_filter.py
+++ event_filter.py
@@ -24,27 +24,3 @@ def get_min_distance(event, lat, lon):
                     coord['wgs84_longitude'], lat, lon)
             min_dist = min(min_dist, dist)
     return min_dist
-
-
-def not_in_undisturbed_window(events, undisturbed_start, undisturbed_end):
-    results = []
-    for e in events:
-        if not e['start_time']:
-            # no start/end time
-            results.append(e)
-        else:
-            start = parse_time_with_second(e['start_time'])
-            end = parse_time_with_second(e['start_time'])
-            ustart = parse_time_without_second(undisturbed_start)
-            uend = parse_time_without_second(uend)
-            if (start < ustart) or (end > uend):
-                results.append(e)
-    return results
-
-
-def parse_time_with_second(time):
-    return datetime.strptime(time, '%H:%M:%S')
-
-
-def parse_time_without_second(time):
-    return datetime.strptime(time, '%H:%M')
